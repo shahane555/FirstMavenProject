84f9c5400dfaa842f2ad3b2bd0544aa1e9ab4f6b
shahane <shahane.manucharyan@mail.ru>
2023-07-10 14:14:01 +0400

    test registration and login funcionality

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class User {
+public class User {
+    private String name;
+    private String surname;
+    private String username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    private String password;
+
+
+    public User(String name, String surname, String username, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof User) {
+            if (this.username.equals(((User) o).username) && this.password.equals(((User) o).password)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
