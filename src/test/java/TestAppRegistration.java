052ab68356f1862414f7c08f9f79095d3e33e0c2
shahane <shahane.manucharyan@mail.ru>
2023-07-10 14:12:09 +0400

    Test registration and login funcionality

--- src/test/java/TestAppRegistration.java
+++ src/test/java/TestAppRegistration.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Test {
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestAppRegistration {
+    User user = new Application().register("name", "surname", "ema@il.com", "12345678");
+    User user2 = new Application().register("Anna", "Poghosyan", "u@sername.com", "55555555");
+    User user4 = new Application().register("name", "surname", "username@2.com", "12345678");
+
+    @Test
+    public void successRegistration() {
+        Assert.assertEquals(new Application().login("ema@il.com", "12345678"), user);
+    }
+
+    @Test
+    public void loginWithCorrectUsername() {
+        Assert.assertEquals(new Application().login("ema@il.com", "12345678"), user);
+    }
+
+    @Test
+    public void loginWithInvalidUsername() {
+        Assert.assertEquals(new Application().login("e@mail.com", "12345678"), user);
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void doubleRegistration() {
+        User user3 = new Application().register("Anna", "Poghosyan", "us@ername.com", "55555555");
+    }
+
+    @DataProvider(name = "symbols")
+    public Object[][] suitableSymbols() {
+        return new Object[][]{{"email@.com"}, {"emailcom"}, {"email@com"}};
+    }
+
+    @Test(dataProvider = "symbols")
+    public void usernameValidation(String username) {
+        User user = new Application().register("name", "surname", username, "12345678");
+        System.out.println(username);
+    }
+
+    @Test
+    public void validPassword() {
+        User user = new Application().register("name", "surname", "User@name", "12345678");
+    }
+
+    @Test
+    public void invalidPassword() {
+        User user = new Application().register("name", "surname", "username", "123");
+    }
+
+    public static void main(String[] args) {
+        Application.getUsers().clear();
+    }
 }
+
--- src/test/java/Users.java
+++ src/test/java/Users.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Users {
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Users {
+    @BeforeTest
+    public static User register(String login, String password) {
+        return new User(login, password);
+    }
+
+    @Test
+    public static User login(String login, String password) {
+        return new User(login, password);
+    }
 }
