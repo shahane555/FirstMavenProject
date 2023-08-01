979dfbae913032449657f96a1d16fd6318477c3f
shahane <shahane.manucharyan@mail.ru>
2023-08-01 16:36:50 +0400

    Test 'search result' quantity

--- src/test/java/amazontest/BooksBarTest.java
+++ src/test/java/amazontest/BooksBarTest.java
@@ -2,28 +2,22 @@ package amazontest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class BooksBarTest {
     private WebDriver driver;
-    private JavascriptExecutor js;
 
     @BeforeMethod
     public void testBasketButton() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        js = (JavascriptExecutor) driver;
         driver.get("https://www.amazon.com/");
     }
 
